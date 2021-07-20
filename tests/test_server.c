06c05c82fe1f42def43f3b317a2257a765b55cb4
Shaya Potter <shaya@redislabs.com>
2021-07-20 14:02:51 +0300

    remove commented out line in test that is no longer needed

--- tests/test_server.c
+++ tests/test_server.c
@@ -1,4 +1,3 @@
-
 #include <stdbool.h>
 #include <assert.h>
 #include <stdlib.h>
@@ -3085,7 +3084,6 @@ void TestRaft_leader_recv_entry_resets_election_timeout(
     raft_set_election_timeout(r, 1000);
     raft_set_state(r, RAFT_STATE_LEADER);
     raft_add_node(r, NULL, 1, 1);
-//    raft_node_set_voting(raft_get_node(r, 1), 1);
 
     raft_periodic(r, 900);
 
