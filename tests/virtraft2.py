60164952a0bf84e792ed39a375a0a9ee46d60f60
Shaya Potter <shaya@redislabs.com>
2021-08-18 11:59:45 +0300

    add a read_queue test to virtraft
    every iteration we push a read_queue request and the handler and "arg" we pass to it set a variable on calling.  we can use this to make sure that the read_queue doesn't get too far from the iteration.
    
    this is analagoues to the current deadlock test.
    
    in the future, we will also test for correctness, that the other nodes confirm to virtraft that they have gotten the msgid that corresponds to allowing this

--- tests/virtraft2.py
+++ tests/virtraft2.py
@@ -222,6 +222,23 @@ def raft_notify_membership_event(raft, udata, node, ety, event_type):
     return ffi.from_handle(udata).notify_membership_event(node, ety, event_type)
 
 
+class ReadQueueEntry(object):
+    def __init__(self, network):
+        self.network = network
+        self.iteration = network.iteration
+
+
+def handle_read_queue(arg, can_read):
+    val = int(ffi.cast("int", arg))
+    if can_read != 0:
+        logger.debug(f"handling read_request {val}")
+        if net.last_read_iteration < val:
+            net.last_read_iteration = val
+    else:
+        logger.debug(f"ignoring read_request {val}")
+
+
+
 def raft_log(raft, node, udata, buf):
     server = ffi.from_handle(lib.raft_get_udata(raft))
     # if server.id in [1] or (node and node.id in [1]):
@@ -292,6 +309,12 @@ class Network(object):
                 assert e == 0 or e == lib.RAFT_ERR_NOT_LEADER
                 break
 
+    def push_read(self):
+        for sv in self.active_servers:
+            if lib.raft_is_leader(sv.raft):
+                logger.debug(f"adding a read_request to {sv.id}")
+                lib.raft_queue_read_request(sv.raft, sv.handle_read_queue, ffi.cast("void *", sv.network.iteration))
+
     def id2server(self, id):
         for server in self.servers:
             if server.id == id:
@@ -342,13 +365,18 @@ class Network(object):
                 server.periodic(self.random.randint(1, 100))
 
         # Deadlock detection
-        if self.latest_applied_log_idx != 0 and self.latest_applied_log_iteration + 5000 < self.iteration:
+        if self.client_rate != 0 and self.latest_applied_log_idx != 0 and self.latest_applied_log_iteration + 5000 < self.iteration:
             logger.error("deadlock detected iteration:{0} appliedidx:{1}\n".format(
                 self.latest_applied_log_iteration,
                 self.latest_applied_log_idx,
                 ))
             self.diagnostic_info()
             sys.exit(1)
+
+        if self.last_read_iteration + 5000 < self.iteration:
+            logger.error("deadlock detected in handling reads: last_read from iteration {0} current at {1}\n".format(
+                self.last_read_iteration, self.iteration,
+            ))
 
         # Count leadership changes
         assert len(self.active_servers) > 0, self.diagnostic_info()
@@ -369,6 +397,7 @@ class Network(object):
         for partition in self.partitions:
             # Partitions are in one direction
             if partition[0] is sendor and partition[1] is sendee:
+                logger.info(f"dropping message from {sendor.id} to {sendee.id}")
                 return
             # duplex partitions prevent both directions from sending traffic
             if self.duplex_partition and (
@@ -789,6 +818,7 @@ class RaftServer(object):
         self.raft_node_has_sufficient_logs = ffi.callback("int(raft_server_t* raft, void *user_data, raft_node_t* node)", raft_node_has_sufficient_logs)
         self.raft_notify_membership_event = ffi.callback("void(raft_server_t* raft, void *user_data, raft_node_t* node, raft_entry_t* ety, raft_membership_e)", raft_notify_membership_event)
         self.raft_log = ffi.callback("void(raft_server_t*, raft_node_id_t, void*, const char* buf)", raft_log)
+        self.handle_read_queue = ffi.callback("void(void *arg, int can_read)", handle_read_queue)
 
     def recv_entry(self, ety):
         # FIXME: leak
@@ -1039,7 +1069,7 @@ class RaftServer(object):
 
     def entry_append(self, ety, ety_idx):
         try:
-            assert not self.fsm_log or self.fsm_log[-1].term <= ety.term
+            assert not self.fsm_log or self.fsm_log[-1].term <= ety.term, f"node {self.id}'s term {self.fsm_log[-1].term} > to entry term {ety.term}"
         except Exception as e:
             self.abort_exception = e
             # FIXME: consider returning RAFT_ERR_SHUTDOWN
@@ -1180,6 +1210,7 @@ if __name__ == '__main__':
             logger.addHandler(fh)
             logger.propagate = False
 
+    global net
     net = Network(int(args['--seed']))
 
     net.dupe_rate = int(args['--dupe_rate'])
@@ -1204,6 +1235,8 @@ if __name__ == '__main__':
     for i in range(0, int(args['--iterations'])):
         net.iteration += 1
         try:
+            net.push_read()
+            net.poll_messages()
             net.periodic()
             net.poll_messages()
         except:
